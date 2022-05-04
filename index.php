6c188c020d792119f253df26c0d954648e12e139
kengo6053 <91479879+kengo6053@users.noreply.github.com>
2022-05-04 15:03:40 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<head></head>
+<body>
+
+こちらはHTMLです。
+
+数字と文字
+数字は半角
+
+<?php
+
+echo('test');
+echo('こんにちは');
+
+$test = '帆井健悟';
+var_dump($test);
+
+$array = [1,2,3];
+
+echo $array[1];
+
+$array_1 = ['ああ',2,3];
+
+$array_2 = [
+    [3,4,5],
+    ['赤','青','黄']
+];
+
+echo $array_2[0][0];
+
+?>
+</body>
+</html>
